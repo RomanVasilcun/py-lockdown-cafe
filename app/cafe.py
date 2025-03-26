68301c7541779a7ae8b1fae60cd0912e070e74d6
Roman Vasilcun <roman.vasilcun@gmail.com>
2025-03-26 12:49:09 +0200

    "Solution"

---
+++ app/cafe.py
@@ -0,0 +1,38 @@
+import datetime
+from .errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
+
+
+class Cafe:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise (
+                NotVaccinatedError(f"{visitor["name"]} "
+                                   f"is not vaccinated."))
+
+        expiration_date = visitor["vaccine"].get("expiration_date")
+        if not isinstance(expiration_date, datetime.date):
+            raise (
+                OutdatedVaccineError(f"{visitor["name"]}'s "
+                                     f"vaccine information is invalid."))
+
+        if expiration_date < datetime.date.today():
+            raise (
+                OutdatedVaccineError
+                (f"{visitor["name"]}'s vaccine expired "
+                 f"on {expiration_date.strftime("%Y-%m-%d")}."))
+
+        if ("wearing_a_mask" not in visitor
+                or not visitor["wearing_a_mask"]):
+            raise (
+                NotWearingMaskError(f"{visitor["name"]} "
+                                    f"is not wearing a mask."))
+
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from .cafe import Cafe
+from .errors import VaccineError, NotWearingMaskError
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    masks_to_buy = 0
+    all_vaccinated = True
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            all_vaccinated = False
+        except NotWearingMaskError:
+            masks_to_buy += 1
+
+    if not all_vaccinated:
+        return "All friends should be vaccinated"
+    elif masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    else:
+        return f"Friends can go to {cafe.name}"
